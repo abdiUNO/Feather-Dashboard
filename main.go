60014755d45f418a9a4ed453b431cf7251975c70
abdullahi <abdullahimahamed0987@gmail.com>
2019-02-05 01:51:47 -0600

    added new templates, css, and funcs

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/abdullahi/go-api/controllers"
+	"github.com/unrolled/render"
 
+	u "github.com/abdullahi/go-api/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -25,6 +29,67 @@ func main() {
 
 	router.HandleFunc("/users", controllers.ShowFakeUsers).Methods("GET")
 	router.HandleFunc("/users/new", controllers.CreateFakeUser).Methods("POST")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ren := render.New(render.Options{
+			Directory: "templates",
+			Layout:    "layout",
+			Funcs: []template.FuncMap{
+				{
+					"myCustomFunc": u.TimeElapsed,
+				},
+			},
+		})
+
+		urlPart := strings.Split(r.URL.Path, "/")
+
+		breadCrumbs := make([]*controllers.BreadCrumb, 0)
+
+		for key, value := range urlPart {
+			crumb := new(controllers.BreadCrumb)
+			crumb.Link = value
+			crumb.Last = (len(urlPart) - 1) == key
+			crumb.Value = strings.Title(value)
+			breadCrumbs = append(breadCrumbs, crumb)
+		}
+
+		data := map[string]interface{}{
+			"BreadCrumbs": breadCrumbs,
+			"Active":      "",
+		}
+
+		err := ren.HTML(w, http.StatusOK, "home", data)
+		fmt.Println(err)
+		if err != nil {
+			u.Respond(w, u.Message(false, "Invalid request"))
+			return
+		}
+	})
+
+	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ren := render.New(render.Options{
+			Directory: "templates",
+			Layout:    "layout",
+			Funcs: []template.FuncMap{
+				{
+					"myCustomFunc": u.TimeElapsed,
+				},
+			},
+		})
+
+		breadCrumbs := make([]*controllers.BreadCrumb, 0)
+
+		data := map[string]interface{}{
+			"BreadCrumbs": breadCrumbs,
+			"Active":      "",
+		}
+
+		err := ren.HTML(w, http.StatusOK, "404", data)
+		fmt.Println(err)
+		if err != nil {
+			u.Respond(w, u.Message(false, "Invalid request"))
+			return
+		}
+	})
 
 	// router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	// router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
